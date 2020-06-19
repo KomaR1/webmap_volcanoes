fb9446bcc294d11e1f1896cfc56a6ae62b621710
KomaR1 <sypachov@gmail.com>
2020-06-19 21:57:46 +0300

    Modifying points

--- my_map.py
+++ my_map.py
@@ -7,6 +7,14 @@ ln = list(data["LON"])
 el = list(data["ELEV"])
 n = list(data["NAME"])
 
+def color(elevation):
+    if elevation < 1500:
+        return 'green'
+    elif elevation > 1500 and elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 html = """
 Volcano name:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
@@ -18,7 +26,8 @@ map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 fg = folium.FeatureGroup(name="My map")
 for lat, lon, elev, name in zip(lt, ln, el, n):
     iframe = folium.IFrame(html=html % (name, name, elev), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lat, lon], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lat, lon], radius=10, popup=folium.Popup(iframe), fill_color=color(elev),
+                                     fill=True, color='blue', fill_opacity=0.7))
 
 map.add_child(fg)
 
