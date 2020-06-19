b717c83b065aac646a30864e704c97a2b2d72414
KomaR1 <sypachov@gmail.com>
2020-06-19 23:40:47 +0300

    Adding layer control

--- my_map.py
+++ my_map.py
@@ -23,18 +23,23 @@ Height: %s m
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My map")
+fgv = folium.FeatureGroup(name="Volcanoes")
+
 for lat, lon, elev, name in zip(lt, ln, el, n):
     iframe = folium.IFrame(html=html % (name, name, elev), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lat, lon], radius=10, popup=folium.Popup(iframe), fill_color=color(elev),
+    fgv.add_child(folium.CircleMarker(location=[lat, lon], radius=10, popup=folium.Popup(iframe), fill_color=color(elev),
                                      fill=True, color='blue', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=(open("world_data.json", 'r', encoding='utf-8-sig').read()),
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=(open("world_data.json", 'r', encoding='utf-8-sig').read()),
                                   style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000
                                   else 'yellow' if 10000000 <= x['properties']['POP2005'] < 50000000
                                   else 'orange' if 50000000 <= x['properties']['POP2005'] < 200000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("map.html")
 
